bf2d5eaac2c579e7611df03a20cc9d831c2945e2
TacosEntail <njt102897@yahoo.com>
2024-09-17 14:10:17 -0500

    work on races

---
+++ script.js
@@ -0,0 +1,8 @@
+function closeContent() {
+    document.getElementById("TableClose").style.display = 'none';
+    document.getElementById("TableOpen").style.display = 'contents';
+}
+function OpenContent() {
+    document.getElementById("TableClose").style.display = 'contents';
+    document.getElementById("TableOpen").style.display = 'none';
+}
